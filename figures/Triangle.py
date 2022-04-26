4eca1bb282d30aeaabad5e34166bfb0d7ce3ae5a
Ivan <94646395+ThanhDoIVan@users.noreply.github.com>
2022-04-26 22:45:57 +0300

    Add files via upload

---
+++ figures/Triangle.py
@@ -0,0 +1,9 @@
+from Figures import Figures
+class Triangle(Figures):
+    def __init__(self, x1, y1, x2, y2, x3, y3):
+        # super() gives access to the methods of parent class
+        super().__init__(x1, y1, x2, y2)
+        self.x3 = x3
+        self.y3 = y3
+    def output(self):
+        print(f'Triangle ([{self.x1}, {self.y1}], [{self.x2}, {self.y2}], [{self.x3}, {self.y3}])')
